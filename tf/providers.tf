af4afbbc1953bbe427e8eb411abb902083dd320c
Jasmit Tarang <jasmit.tarang@goteleport.com>
2025-03-17 15:46:50 -0500

    try using Teleport github actions

--- tf/providers.tf
+++ tf/providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = var.aws_region
-  profile                 = "${var.aws_profile_name}"
+  profile                 = var.aws_profile_name
   default_tags {
     tags = var.tags
   }
