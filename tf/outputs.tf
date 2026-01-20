9413e3f77ee30a2045cc500d0535b9a2003c9f73
Jasmit Tarang <jasmit.tarang@goteleport.com>
2026-01-20 13:03:27 -0600

    Update outputs.tf

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,7 +1,3 @@
-output "api_gateway_url" {
-  value = aws_api_gateway_deployment.api_deployment.invoke_url
-}
-
 output "api_key" {
   value     = aws_api_gateway_api_key.cost_api_key.value
   sensitive = true
